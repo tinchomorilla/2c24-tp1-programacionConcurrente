10d8bc4df393345a4dcb02bd136331b55383f072
Tincho Morilla <mmorilla@fi.uba.ar>
2024-09-09 00:32:59 -0300

    se visualizan el promedio de las distancias por pantalla

--- tp-fork-join/src/main.rs
+++ tp-fork-join/src/main.rs
@@ -74,37 +74,85 @@ fn main() -> std::io::Result<()> {
         let line = l.unwrap();
         let fields: Vec<&str> = line.split(',').collect();
         let mut counts = HashMap::new();
+        let mut distances = HashMap::new();
 
-        if let Some(killed_by) = fields.first() {
-            let number_of_deaths_caused = counts.entry(killed_by.to_string()).or_insert(0);
+        if let Some(weapon) = fields.first() {
+            let number_of_deaths_caused = counts.entry(weapon.to_string()).or_insert(0);
             *number_of_deaths_caused += 1;
         }
-        counts
+
+        if let (Some(weapon), Some(killer_x), Some(killer_y), Some(victim_x), Some(victim_y)) = (
+            fields.first(),
+            fields.get(3).and_then(|x| x.parse::<f64>().ok()),
+            fields.get(4).and_then(|y| y.parse::<f64>().ok()),
+            fields.get(10).and_then(|x| x.parse::<f64>().ok()),
+            fields.get(11).and_then(|y| y.parse::<f64>().ok()),
+        ) {
+            let distance = ((killer_x - victim_x).powi(2) + (killer_y - victim_y).powi(2)).sqrt();
+            let distances_vec = distances.entry(weapon.to_string()).or_insert(Vec::new());
+            distances_vec.push(distance);
+        }
+
+        (counts, distances)
     });
 
     // Reduce todos los HashMaps a un solo HashMap, que contiene todas las claves juntas (armas)
     // y sus valores acumulados.
     // result = { "arma1": 10, "arma2": 20, ...}
     let result = mapped_iter.reduce(
-        HashMap::new,
-        |mut acc, map| {
-            map.iter().for_each(|(k, v)| {
-                let count = acc.entry(k.to_string()).or_insert(0);
+        || (HashMap::new(), HashMap::new()),
+        |(mut acc_counts, mut acc_distances), (counts, distances)| {
+            counts.iter().for_each(|(k, v)| {
+                let count = acc_counts.entry(k.to_string()).or_insert(0);
                 *count += v;
             });
-            acc
+
+            distances.iter().for_each(|(k, v)| {
+                acc_distances
+                    .entry(k.to_string())
+                    .or_insert(Vec::new())
+                    .extend(v);
+            });
+
+            (acc_counts, acc_distances)
         },
     );
 
     let duration = start.elapsed();
 
+    let (counts, distances) = result;
+
     println!("Tiempo total de lectura: {:?}", duration);
 
-    let mut top_weapons: Vec<(&String, &i32)> = result.iter().collect();
+    let mut top_weapons: Vec<(&String, &i32)> = counts.iter().collect();
     top_weapons.sort_by(|a: &(&String, &i32), b| b.1.cmp(a.1));
 
+    // Nombres de las 10 armas principales
+    let top_10_weapons: Vec<&String> = top_weapons
+        .iter()
+        .take(10)
+        .map(|(weapon, _)| *weapon)
+        .collect();
+
+    // Filtrar las distancias para las 10 armas principales
+    let top_10_distances: HashMap<String, Vec<f64>> = distances
+        .iter()
+        .filter(|(k, _)| top_10_weapons.contains(k))
+        .map(|(k, v)| (k.clone(), v.clone()))
+        .collect();
+
+    // Calcular el promedio de distancias para cada arma en el top 10
+    let average_distances: HashMap<String, f64> = top_10_distances
+        .iter()
+        .map(|(k, v)| {
+            let sum: f64 = v.iter().sum();
+            let count = v.len() as f64;
+            (k.clone(), sum / count)
+        })
+        .collect();
+
     // Calcular el total de muertes
-    let total_deaths: i32 = result.values().sum();
+    let total_deaths: i32 = counts.values().sum();
 
     println!("Top 10 armas que causaron más muertes:");
     for (weapon, count) in top_weapons.iter().take(10) {
@@ -113,8 +161,11 @@ fn main() -> std::io::Result<()> {
         println!("{}: {} ({:.2}%)", weapon, count, rounded_percentage);
     }
 
-
- 
+    // Imprimir las distancias promedio para las 10 armas principales
+    println!("Promedio de distancias para las top 10 armas:");
+    for (weapon, avg_distance) in average_distances.iter() {
+        println!("{}: {:.2}", weapon, avg_distance);
+    }
 
     Ok(())
 }
